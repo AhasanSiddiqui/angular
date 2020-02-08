00c9bf9c50963b0c8411098314a734ffd429e3f3
Ahasan Siddiqui <ahasansiddiqui@gmail.com>
2020-02-08 16:57:35 +0530

    interacting with HttpServices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { PostComponent } from './post/post.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    ContactFormComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
